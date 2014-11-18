91278d5ec7319fa07faaefd9b73c31e6878aa7a1
Rich Seviora <richard.seviora@gmail.com>
2014-11-18 12:18:22 -0500

    Completed Missing Objects Exercises

--- js_fundamentals/objects.js
+++ js_fundamentals/objects.js
@@ -30,13 +30,33 @@ function Recipe (title, servings, ingredients) {
 var newRecipe = new Recipe('Caesar',1,['Brutus','One Knife']);
 newRecipe.log();
 
-
-
 // EXERCISE: The Reading List
 
 // Keep track of which books you read and which books you want to read!
-
 // Create an array of objects, where each object describes a book and has properties for the title (a string), author (a string), and alreadyRead (a boolean indicating if you read it yet).
+
+function Book (title, author, alreadyRead) {
+    this.title = title;
+    this.author = author;
+    this.alreadyRead = alreadyRead;
+    this.describe = function () {
+        if (this.alreadyRead) {
+            console.log("You already read '" + this.title + "' by " + this.author);
+        } else {
+            console.log("You still need to read '" + this.title + "' by " + this.author);
+        }
+    }
+}
+
+var newBook = new Book('Lord of the Rings', 'JRR Tolkien', true);
+var newBook1 = new Book('Whatever', 'Someone', false);
+var bookArray = [newBook, newBook1];
+
+for (var i = 0; i < bookArray.length; i++) {
+    bookArray[i].describe();
+}
+
+
 // Iterate through the array of books. For each book, log the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
 // Now use an if/else statement to change the output depending on whether you read it yet or not. If you read it, log a string like 'You already read "The Hobbit" by J.R.R. Tolkien', and if not, log a string like 'You still need to read "The Lord of the Rings" by J.R.R. Tolkien.'
 
@@ -47,5 +67,15 @@ newRecipe.log();
 // Create an object to store the following information about your favorite movie: title (a string), duration (a number), and stars (an array of strings).
 // Create a function to print out the movie information like so: "Puff the Magic Dragon lasts for 30 minutes. Stars: Puff, Jackie, Living Sneezes."
 
+function Movie(title,duration,stars) {
+    this.title = title;
+    this.duration = duration;
+    this.stars = stars;
+    this.describe = function () {
+        console.log (this.title + ' lasts for ' + this.duration + ' minutes. Stars: ' + stars.join(', ') + '.');
+    }
+}
 
+var minorityReport = new Movie ('Minority Report', 120, ['Tom Cruise', 'Some Other People']);
 
+minorityReport.describe();
