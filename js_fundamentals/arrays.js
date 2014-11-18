bd8038c3fdd2534ae912cbba227107295b2797f7
Rich Seviora <richard.seviora@gmail.com>
2014-11-18 11:50:36 -0500

    Finished Arrays Exercise

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -6,5 +6,22 @@
 // For each choice, log to the screen a string like: "My #1 choice is blue."
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
+var array = ['cats','dogs','birds','fish'];
 
+function ordinalize(number) {
+    var i = number % 10;
+    var j = number % 100;
+    if (i == 1 && j != 11) {
+        return number + 'st';
+    } else if (i == 2 && j != 12) {
+        return number + 'nd';
+    } else if (i == 3 && j != 13) {
+        return number + 'rd';
+    } else {
+        return number + 'th';
+    }
+}
 
+for (var i = 0; i < array.length; i++) {
+    console.log ('My ' + ordinalize(i + 1) + ' choice is ' + array[i]);
+}
